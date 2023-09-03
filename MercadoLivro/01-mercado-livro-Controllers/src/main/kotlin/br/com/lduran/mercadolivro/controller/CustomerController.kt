ec0c15fe2eb25d55e15df828246bf5ac33421d53
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2023-09-03 12:59:27 -0300

    Projeto MercadoLivro
    - Retornando um Customer no GET;
    - Implementando o Post Customer;
    - Alterando o Status de Retorno;
    - Refatorando um pouco;
    - Recebendo um Path Param;
    - Implementando PUT;
    - Implementando DELETE;
    - Adicionando query params;

--- MercadoLivro/01-mercado-livro-Controllers/src/main/kotlin/br/com/lduran/mercadolivro/controller/CustomerController.kt
+++ MercadoLivro/01-mercado-livro-Controllers/src/main/kotlin/br/com/lduran/mercadolivro/controller/CustomerController.kt
@@ -1,20 +1,53 @@
 package br.com.lduran.mercadolivro.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import br.com.lduran.mercadolivro.controller.request.PostCustomerRequest
+import br.com.lduran.mercadolivro.controller.request.PutCustomerRequest
+import br.com.lduran.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customers")
 class CustomerController {
+    val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun helloWorld(): String {
-        return "Customer 1"
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+        return customers
     }
 
-    @GetMapping("/2")
-    fun customer2(): String {
-        return "Customer 2"
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest) {
+        val id = if(customers.isEmpty()) {
+            1
+        } else{
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        customers.filter { it.id == id }.first().let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customers.removeIf { it.id == id }
     }
 }
