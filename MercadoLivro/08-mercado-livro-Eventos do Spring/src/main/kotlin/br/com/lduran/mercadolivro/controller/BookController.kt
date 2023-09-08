4fef010d261172b77afe103935fd1d94cbd1960d
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2023-09-08 15:46:08 -0300

    Projeto MercadoLivro
    - Eventos do Spring:
      - Inclusão das annotations @Tag e @Operation para explicar o propósito de cada endpoint

--- MercadoLivro/08-mercado-livro-Eventos do Spring/src/main/kotlin/br/com/lduran/mercadolivro/controller/BookController.kt
+++ MercadoLivro/08-mercado-livro-Eventos do Spring/src/main/kotlin/br/com/lduran/mercadolivro/controller/BookController.kt
@@ -7,6 +7,8 @@ import br.com.lduran.mercadolivro.extension.toBookModel
 import br.com.lduran.mercadolivro.extension.toResponse
 import br.com.lduran.mercadolivro.service.BookService
 import br.com.lduran.mercadolivro.service.CustomerService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -16,23 +18,28 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/books")
+@Tag(name = "Books", description = "Endpoints for Managing Books")
 class BookController(val bookService : BookService,
                      val customerService: CustomerService) {
     @GetMapping
+    @Operation(summary = "Finds all Books", description = "Finds all Books", tags = arrayOf("Books"))
     fun findAll(@PageableDefault(page= 0, size= 10) pageable: Pageable, @RequestParam(required = false) name: String?): Page<BookResponse> {
         return bookService.findAll(pageable,name).map { it.toResponse() }
     }
 
     @GetMapping("/actives")
+    @Operation(summary = "Finds all Books that are active", description = "Finds all Books that are active", tags = arrayOf("Books"))
     fun findActives(@PageableDefault(page= 0, size= 10) pageable: Pageable): Page<BookResponse> = bookService.findActives(pageable).map { it.toResponse()}
 
     @GetMapping("/{id}")
+    @Operation(summary = "Finds a Book", description = "Finds a Book", tags = arrayOf("Books"))
     fun findById(@PathVariable id: Int): BookResponse {
         return bookService.findById(id).toResponse()
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Adds a new Book", description = "Adds a new Book by passing in a JSON representation of the book!", tags = arrayOf("Books"))
     fun create(@RequestBody @Valid book: PostBookRequest) {
         val customer= customerService.findById(book.customerId)
         bookService.create(book.toBookModel(customer))
@@ -40,12 +47,14 @@ class BookController(val bookService : BookService,
 
     @PostMapping("/list")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Adds a list of new Books", description = "Adds a list of new Books by passing in a JSON representation of the book!", tags = arrayOf("Books"))
     fun createList(@RequestBody @Valid books: List<PostBookRequest>) {
         bookService.createList(books)
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Updates a Book", description = "Updates a Book by passing in a JSON, XML or YML representation of the book!", tags = arrayOf("Books"))
     fun update(@PathVariable id: Int, @RequestBody @Valid book: PutBookRequest) {
         val bookSaved= bookService.findById(id)
         bookService.update(book.toBookModel(bookSaved))
@@ -53,6 +62,7 @@ class BookController(val bookService : BookService,
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deletes a Book", description = "Deletes a Book by passing in a JSON, XML or YML representation of the book!", tags = arrayOf("Books"))
     fun delete(@PathVariable id: Int) {
         bookService.delete(id)
     }
--- MercadoLivro/08-mercado-livro-Eventos do Spring/src/main/kotlin/br/com/lduran/mercadolivro/controller/PurchaseController.kt
+++ MercadoLivro/08-mercado-livro-Eventos do Spring/src/main/kotlin/br/com/lduran/mercadolivro/controller/PurchaseController.kt
@@ -5,6 +5,8 @@ import br.com.lduran.mercadolivro.controller.request.PostPurchaseRequest
 import br.com.lduran.mercadolivro.controller.response.PurchaseResponse
 import br.com.lduran.mercadolivro.service.CustomerService
 import br.com.lduran.mercadolivro.service.PurchaseService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -14,18 +16,23 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/purchases")
+@Tag(name = "Purchases", description = "Endpoints for Managing Purchases")
 class PurchaseController(private val purchaseService: PurchaseService,
                          private val purchaseMapper: PurchaseMapper,
                          private val customerService: CustomerService
 ) {
 
     @GetMapping("/{id}")
+    @Operation(summary = "Finds all Purchases for a Customer", description = "Finds all Purchases for a Customers", tags = arrayOf("Purchases"))
     fun findAllByCustomer(@PageableDefault(page= 0, size= 10) pageable: Pageable, @PathVariable id: Int): Page<PurchaseResponse> {
         val customer = customerService.findById(id)
         return purchaseService.findByCustomer(pageable,customer).map { purchaseMapper.toResponse(it) }}
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Adds a new Purchase",
+               description = "Adds a new Purchase by passing in a JSON representation of the purchase!",
+               tags = arrayOf("Purchases"))
     fun create(@RequestBody @Valid request: PostPurchaseRequest) {
         purchaseService.create(purchaseMapper.toModel(request))
     }
